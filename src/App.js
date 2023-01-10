74bf0546b9905aed7037c1d8974ca05da7635845
Marcos Amaya <marcos.amaya@usach.cl>
2023-01-10 14:27:18 -0300

    v3 prueba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Prueba React.
+          Prueba React v3. Con commit para GitHub.
         </p>
         
       </header>
