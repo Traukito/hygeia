a2ea47a04dfd08c3e63601f0744388747b138a26
Marcos Amaya <marcos.amaya@usach.cl>
2023-01-10 16:25:12 -0300

    v4 con autenticación

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "hygeiab6a2edf3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// Importaciones para agergar autenticación, según la página de documentación
+// de AWS
+// import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+
+
+function App( { signOut, user } ) {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Prueba React v3. Con commit para GitHub.
-        </p>
-        
+          <Heading style={{color:'white'}}level={1}>Hola {user.username}</Heading>
+        </p>        
+        <Button style={{color:'white'}} onClick={signOut}>Salir</Button>
       </header>
+
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
