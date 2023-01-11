990c206a5575e26b0b459b11af64b518d5198059
Marcos Amaya <marcos.amaya@usach.cl>
2023-01-10 23:44:26 -0300

    intentando con autenticación v7

--- src/App.js
+++ src/App.js
@@ -4,26 +4,21 @@ import './App.css';
 // Importaciones para agergar autenticación, según la página de documentación
 // de AWS
 // import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
-import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
 
 
-
-function App( { signOut, user } ) {
+function App() {
   return (
     <div className="App">
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Prueba React v3. Con commit para GitHub.
-          <Heading style={{color:'white'}}level={1}>Hola {user.username}</Heading>
+          Prueba React vX. Con commit para GitHub.        
         </p>        
-        <Button style={{color:'white'}} onClick={signOut}>Salir</Button>
       </header>
 
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
